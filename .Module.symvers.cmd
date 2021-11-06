328b1ed6740ece5a8f51f26a05d9889eee3dd4e3
wuyuanpei <1901188454@qq.com>
2021-11-06 19:43:16 -0400

    send packet

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/richardwu/Desktop/KeyloggerRootkit/Module.symvers := sed 's/ko$$/o/' /home/richardwu/Desktop/KeyloggerRootkit/modules.order | scripts/mod/modpost -m -a   -o /home/richardwu/Desktop/KeyloggerRootkit/Module.symvers -e -i Module.symvers   -T -
